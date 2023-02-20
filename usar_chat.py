bfb28fc678a2a217994ea945748f163093b382f9
mgomez1289 <migue.angel3084@gmail.com>
2023-02-20 11:32:30 -0500

    usar_chat

---
+++ usar_chat.py
@@ -0,0 +1,14 @@
+# ejercicio usar chat
+usuario1 = str ("gerardo")
+usuario2 = str ("aubameyan")
+mensaje = str ("te gusta la que ya tu sabe")
+respuesta = str ("solo las chicas")
+plataforma = str ("whatsapp")
+hora = float (8.45)
+
+print ("nuevo mensaje de", plataforma)
+print (usuario1, "para", usuario2)
+print (usuario1, ":", mensaje)
+print ("a las", hora)
+print ("respondiste:")
+print (usuario2, ":",respuesta)
